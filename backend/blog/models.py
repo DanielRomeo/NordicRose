75d09944cda0ad606bc2a47c1b52d6c17c021f85
danielromeo <danielromeo99@gmail.com>
2024-01-04 15:00:25 +0200

    added pillow dependencies and steady working to make backend take images that are uploaded

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Item(models.Model): # i added this models.Model when compiler complained about irretability
+class Author(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    facebook = models.URLField(max_length=200)
+    twitter = models.URLField(max_length=200)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    def __str__(self) -> str:
+        return self.lastname
+
+class Blogpost(models.Model): # i added this models.Model when compiler complained about irretability
     title = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
